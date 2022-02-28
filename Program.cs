5627b4e7adc7cf94d4feff907b2910b525bd51d7
Nicklas Matthiesen Jensen <nicej1991@gmail.com>
2022-02-28 20:49:57 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿global using OOPH1Aflevering.Codes;
+
+Console.WriteLine("Hello, World!");
+
+//MotorKøretøj motorKøretøj = new MotorKøretøj();
+//MotorKøretøj motorKøretøj = new Bil();
+//MotorKøretøj motorKøretøj = new Audi();
+//Audi audi = new Audi();
+//Audi audi = new Bil();
+//Audi audi = new MotorKøretøj();
+
+// de første 4 vil ikke vise fejl
+
+Console.ReadKey();
